6f166a6c1bed23b9fa4139d9aa8a0c9fa5f6357f
gvarun22 <gvarun22@gmail.com>
2019-05-28 15:18:23 -0700

    Add user permissions

--- FormatDisk.ps1
+++ FormatDisk.ps1
@@ -1,2 +1,7 @@
 Get-Disk | Where-Object PartitionStyle -eq "RAW" | Initialize-Disk -PartitionStyle MBR -Confirm:$False -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$False
+
 New-Item -ItemType directory -Path D:\source
+$acl = Get-Acl D:source
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Users","FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
+$acl.AddAccessRule($rule)
+Set-Acl D:source $acl
