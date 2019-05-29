ed418ba884424dd1c5f590fe5e6dbf962d03c51e
gvarun22 <gvarun22@gmail.com>
2019-05-29 13:37:40 -0700

    Update FormatDisk.ps1

--- FormatDisk.ps1
+++ FormatDisk.ps1
@@ -1,5 +1,6 @@
 Get-Disk | Where-Object PartitionStyle -eq "RAW" | Initialize-Disk -PartitionStyle MBR -Confirm:$False -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$False
 
+New-LocalGroup ServiceFabricAllowedUsers
 New-Item -ItemType directory -Path D:\source
 $acl = Get-Acl D:source
 $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("ServiceFabricAllowedUsers","FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
